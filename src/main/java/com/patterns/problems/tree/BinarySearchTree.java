bf5f2c14bd68f81fad4536cf1e3a2b69160ab675
jackie <mgjackline@gmail.com>
2024-12-23 22:48:37 +0300

    add methods

--- src/main/java/com/patterns/problems/tree/BinarySearchTree.java
+++ src/main/java/com/patterns/problems/tree/BinarySearchTree.java
@@ -29,7 +29,58 @@ public class BinarySearchTree {
         return false;
     }
 
-    public boolean insert(int value){
+    public boolean rContains(Node currentNode, int value){
+        if (currentNode == null) return false;
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        }
+        else  {
+            return rContains(currentNode.right, value);
+
+        }
+    }
+
+    public Node insert(Node currentNode, int value){
+        if (currentNode == null){
+            return new Node(value);
+        }
+        if (value < currentNode.value){
+            currentNode.left = insert(currentNode.left, value);
+        } else if (value > currentNode.value){
+            currentNode.right = insert(currentNode.right, value);
+        }
+        return currentNode;
+
+    }
+
+
+        public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+    public void rInsert(int value){
+        if (root ==null) root = new Node(value);
+
+        rContains(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value){
+        if (currentNode == null){
+            return new Node(value);
+        }
+        if(  value < currentNode.value){
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if(  value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+
+        }
+        return currentNode;
+
+    }
+
+        public boolean insert(int value){
         Node newNode = new Node(value);
         if (root ==null){
             root = newNode;
@@ -68,5 +119,13 @@ public class BinarySearchTree {
             }
         }
         return false;
+    }
+
+    private Integer  minValue(Node currentNode){
+        if(currentNode ==null) return null;
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
     }
 }
