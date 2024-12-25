688ef3f99cb1ab93f3cbfa6835b4e8334235a274
jackie <mgjackline@gmail.com>
2024-12-25 15:50:13 +0300

    add sorting algorithmns

---
+++ src/main/java/com/patterns/problems/sorting/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.patterns.problems.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+//    List<Integer> heap;
+
+    public static void bubble(int[] array){
+        for (int x=array.length-1; x>0; x--){
+            for (int j=0; j<x; j++){
+                if(array[j] > array[j+1] ){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1]  = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void bubbleSort(int[] array){
+        for (int x=array.length-1; x>0; x--){
+            for (int j=0; j<x; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] arg){
+        int[] array = new int[]{9,5,2,8,1,7};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
---
+++ src/main/java/com/patterns/problems/sorting/SelectionSort.java
@@ -0,0 +1,47 @@
+package com.patterns.problems.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] array){
+
+        for (int x=0; x< array.length; x++){
+            int minIndex = x;
+            for (int j=x+1; j<array.length; j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if (minIndex != x){
+                int temp = array[x];
+                array[x] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+
+    }
+
+    public static void selection (int[] array){
+        for (int x=0; x<array.length; x++){
+            int minIndex = 0;
+            for (int j=x; j<array.length; j++){
+                if (array[minIndex] > array[j]){
+                    minIndex = array[j];
+                }
+            }
+            if (minIndex != array[x]){
+                int temp = array[minIndex];
+                array[minIndex] = array[x];
+                array[x] = temp;
+            }
+        }
+
+    }
+
+    public static void main(String[] arg){
+        int[] array = new int[]{9,5,2,8,1,7};
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
