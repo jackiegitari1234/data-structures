4b066813696acc17122a5fe33b7dab4317c42336
jackie <mgjackline@gmail.com>
2025-01-12 14:35:26 +0300

    update palidrome

--- src/main/java/com/patterns/problems/palindrome/isPalindrome.java
+++ src/main/java/com/patterns/problems/palindrome/isPalindrome.java
@@ -3,13 +3,14 @@ package com.patterns.problems.palindrome;
 import java.util.HashMap;
 import java.util.Map;
 
+//two pointer
 public class isPalindrome {
 
 
     public static  boolean checkIfPalindrome(String input){
         int x =0;
         int y = input.length()-1;
-        while ( x< input.length() && y> 0 ){
+        while ( x< input.length() && y> input.length()/2 ){
             if (y==x){
                 return true;
             }
@@ -29,7 +30,7 @@ public class isPalindrome {
     public static void main(String[] args) {
 //        String S = "dd";
 //        String S = "fknfkn"; //2
-        String S = "a";
+        String S = "RACECAR";
 //        String S = "aaaabc"; //2
         System.out.println("Is palindromes: " + checkIfPalindrome(S));
     }
