c201a08dd8d5f494e85c5c8e4de1bee4daf79c2e
jackie <mgjackline@gmail.com>
2025-02-03 10:30:51 +0300

    add sum without operator

---
+++ src/main/java/com/patterns/problems/cases/SumWithoutOperator.java
@@ -0,0 +1,34 @@
+package com.patterns.problems.cases;
+
+//& (bitwise AND)
+//| (bitwise OR)
+//^ (bitwise XOR)
+// << (left shift)
+//>> (right shift)
+//~ (bitwise NOT) takes one number and inverts all bits of it.
+public class SumWithoutOperator {
+
+    public int getDifference(int a, int b) {
+        // Compute -b using two's complement
+//        b = ~b + 1;
+
+        return getSum(a, getSum(~b, 1));
+    }
+
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        SumWithoutOperator solution = new SumWithoutOperator();
+        System.out.println("sum " +solution.getSum(5, 3));  // Output: 8
+        System.out.println(solution.getDifference(5, 3));
+//        System.out.println(solution.getSum(-2, -3)); // Output: -5
+    }
+}
+
