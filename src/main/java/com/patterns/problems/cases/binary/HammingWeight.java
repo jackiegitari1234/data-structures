824e297b832408ca65ccaa566b35ebce447ba6b9
jackie <mgjackline@gmail.com>
2025-02-06 09:53:00 +0300

    add more cases

--- src/main/java/com/patterns/problems/cases/binary/HammingWeight.java
+++ src/main/java/com/patterns/problems/cases/binary/HammingWeight.java
@@ -1,5 +1,10 @@
 package com.patterns.problems.cases.binary;
 
+//Given a positive integer n, write a function that returns the number of
+//set bits
+// in its binary representation (also known as the Hamming weight).
+//
+//
 public class HammingWeight {
 
     public static int countSetBits(int n) {
