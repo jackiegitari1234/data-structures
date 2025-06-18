9e1ed86621da6c298abc7e6f8a029b135cfc59ff
JMGITARI <jackline.gitari@vodafone.com>
2025-06-18 11:22:09 +0300

    add solutions

---
+++ src/main/java/com/patterns/others/Testing.java
@@ -0,0 +1,41 @@
+package com.patterns.others;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Testing {
+
+    record DistinctByLength(String str) {
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof DistinctByLength(String other)
+                && str.length() == other.length();
+        }
+
+
+        @Override
+        public int hashCode() {
+            return str == null ? 0 : Integer.hashCode(str.length());
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+        var result = Stream.of("foo", "bar", "baz", "quux")
+            .map(DistinctByLength::new)
+            .distinct()
+            .map(DistinctByLength::str)
+            .toList();
+
+
+
+        long numberOfWords =
+            Stream.of("the", "", "fox", "jumps", "over", "the", "", "dog")  // (1)
+                .filter(Predicate.not(String::isEmpty))
+                .count();
+        System.out.println(numberOfWords); // Output: 4
+    }
+}
--- src/main/java/com/patterns/problems/cases/dynamic/programming/LongestIncreasingSubsequence.java
+++ src/main/java/com/patterns/problems/cases/dynamic/programming/LongestIncreasingSubsequence.java
@@ -12,17 +12,17 @@ public class LongestIncreasingSubsequence {
         if (nums == null || nums.length == 0) return 0;
 
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1); // Every element is an increasing subsequence of length 1
+        int[] options = new int[nums.length];
+        Arrays.fill(options, 1); // Every element is an increasing subsequence of length 1
 
         int maxLength = 1;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    options[i] = Math.max(options[i], options[j] + 1);
                 }
             }
-            maxLength = Math.max(maxLength, dp[i]);
+            maxLength = Math.max(maxLength, options[i]);
         }
         return maxLength;
     }
