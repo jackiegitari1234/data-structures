16051f946d2072ddd5b1704589a3cd34f47e61ce
jackie <mgjackline@gmail.com>
2025-10-18 14:18:01 +0300

    updates

--- src/main/java/com/patterns/problems/recusivebst/BinarySearchTree.java
+++ src/main/java/com/patterns/problems/recusivebst/BinarySearchTree.java
@@ -3,52 +3,41 @@ package com.patterns.problems.recusivebst;
 public class BinarySearchTree {
     Node root;
 
-    boolean insert (int value){
+    Node rInsert (Node currentNode, int value){
+        if (currentNode == null) return new Node(value);
+        if (value < currentNode.value){
+            currentNode.left = rInsert( currentNode.left, value);
+        } else if (value > currentNode.value){
+            currentNode.right = rInsert( currentNode.right, value);
+        }
+        return currentNode;
+
+    }
+
+    void rInsert (int value){
         Node newNode = new Node(value);
         if (root == null){
             root = newNode;
-            return true;
+            return;
         }
-        Node temp = root;
-        while (true) {
-            if (value == temp.value) {
-                return false;
-            }
-            if (value < temp.value) {
-                if (temp.left == null){
-                    temp.left = newNode;
-                    return true;
-                }
-                temp = temp.left;
+         rInsert(root, value);
 
-            } else {
-                if (temp.right == null){
-                    temp.right = newNode;
-                    return true;
-                }
-                temp = temp.right;
-            }
+    }
+
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
         }
     }
 
-    boolean contains (int value){
-        //this line one is not needed
-        if (root == null) return false;
-        Node temp = root;
-        while (temp != null){
-            if (temp.value == value){
-                return true;
-            } else {
-                if (value < temp.value){
-                    temp = temp.left;
-                } else {
-                    temp = temp.right;
-                }
-            }
-        }
-        return false;
+    public boolean rContains(int value) { return rContains(root, value); }
 
-    }
 
     class Node{
         int value;
@@ -58,4 +47,31 @@ public class BinarySearchTree {
             this.value = value;
         }
     }
-}
+
+    Node rDelete (Node currentNode, int value){
+        if (currentNode == null) return null;
+        if (value < currentNode.value){
+            currentNode.left = rDelete( currentNode.left, value);
+        } else if (value > currentNode.value){
+            currentNode.right = rDelete( currentNode.right, value);
+        } else {
+            if (currentNode.left == null && currentNode.right==null){
+                return null;
+            } else if (currentNode.left == null) {
+                return currentNode.right;
+                
+            }else if (currentNode.right == null) {
+                return currentNode.left;
+            }else {
+
+            }
+        }
+        return currentNode;
+
+    }
+
+    Node rDelete (int value){
+        if (root == null) return null;
+        return rDelete(root, value);
+    }
+    }
